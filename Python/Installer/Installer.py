8af92667bd1cd0afe3a0d9e1ee8a33015b24847a
justkevinyt11 <stephenskevin858@gmail.com>
2022-03-31 14:49:05 -0400

    Added Minecraft installer, and fixxed some bugs

--- Python/Installer/Installer.py
+++ Python/Installer/Installer.py
@@ -1,23 +1,18 @@
 import os
 def main():
-
+    tools = ['OneX', 'VsCode', 'Obs', 'HTop', 'Tool-X', 'Neofetch', 'Minecraft']
 
     os.system('clear')
     print("tools")
-    print("1. OneX (hacking tools)")
-    print("2. VsCode (Uses snap)")
-    print("3. Obs")
-    print("4. HTop")
-    print("5. Tool-X (hacking tools)")
-    print("6. Neofetch")
-    print("X. Exit")
-
+    for index, tool in enumerate(tools, start=1):
+        print (index, tool)
+    print ('x Exit')
 
 main()
 inputMain = input("Choose Tool: \n")
 
 
-if inputMain == "x":
+if inputMain == "x" or inputMain == "X":
     os.system('clear')
     exit()
 
@@ -25,17 +20,18 @@ if inputMain == "x":
 if int(inputMain) == 1:
     #OneX Installer
     InputUpdate = input("update system? \n")
-    if InputUpdate == "yes":
+    if InputUpdate == "yes" or InputUpdate == "Yes":
         os.system('sudo apt update && sudo apt upgrade')
     else: pass
     os.system('sudo apt install git')
     os.system('git clone https://github.com/rajkumardusad/onex.git')
     os.system('chmod +x onex/install')
     inputOpen = input ("attempt to open OneX? \n")
-    if inputOpen == "yes":
+    if inputOpen == "yes" or inputOpen == "Yes":
         os.system('./onex/install')
     AskClose = input("close installer? \n")
-    if AskClose == "yes":
+    if AskClose == "yes" or AskClose == "Yes":
+        os.system('clear')
         exit()
     else: os.system('python3 Installer.py')
 
@@ -43,14 +39,15 @@ if int(inputMain) == 1:
 if int(inputMain) == 2:
     #VsCode Installer
     InputUpdate = input("update system? \n")
-    if InputUpdate == "yes":
+    if InputUpdate == "yes" or InputUpdate == "Yes":
         os.system('sudo apt update && sudo apt upgrade')
     else:
         pass
     os.system('sudo apt install snapd')
     os.system('sudo snap install code --classic')
     AskClose = input("close installer? \n")
-    if AskClose == "yes":
+    if AskClose == "yes" or AskClose == "Yes":
+        os.system('clear')
         exit()
     else: os.system('python3 Installer.py')
 
@@ -58,13 +55,14 @@ if int(inputMain) == 2:
 if int(inputMain) == 3:
     #Obs Installer
     InputUpdate = input("update system? \n")
-    if InputUpdate == "yes":
+    if InputUpdate == "yes" or InputUpdate == "Yes":
         os.system('sudo apt update && sudo apt upgrade')
     else:
         pass
     os.system('sudo apt install obs-studio')
     AskClose = input("close installer? \n")
-    if AskClose == "yes":
+    if AskClose == "yes" or AskClose == "Yes":
+        os.system('clear')
         exit()
     else: os.system('python3 Installer.py')
 
@@ -72,13 +70,14 @@ if int(inputMain) == 3:
 if int(inputMain) == 4:
     #Htop Installer
     InputUpdate = input("update system? \n")
-    if InputUpdate == "yes":
+    if InputUpdate == "yes" or InputUpdate == "Yes":
         os.system('sudo apt update && sudo apt upgrade')
     else:
         pass
     os.system('sudo apt install htop')
     AskClose = input("close installer? \n")
-    if AskClose == "yes":
+    if AskClose == "yes" or AskClose == "Yes":
+        os.system('clear')
         exit()
     else:
         os.system('python3 Installer.py')
@@ -87,7 +86,7 @@ if int(inputMain) == 4:
 if int(inputMain) == 5:
     #tool-x installer
     InputUpdate = input("update system? \n")
-    if InputUpdate == "yes":
+    if InputUpdate == "yes" or InputUpdate == "Yes":
         os.system('sudo apt update && sudo apt upgrade')
     else:
         pass
@@ -96,12 +95,13 @@ if int(inputMain) == 5:
     os.system('cd Tool-X')
     os.system('chmod +x install')
     askOpen = input("start Tool-X installer? \n")
-    if askOpen == "yes":
+    if askOpen == "yes" or askOpen == "Yes":
         os.system('./install')
     else:
         pass
     AskClose = input("close installer? \n")
-    if AskClose == "yes":
+    if AskClose == "yes" or AskClose == "Yes":
+        os.system('clear')
         exit()
     else:
         os.system('python3 Installer.py')
@@ -109,18 +109,37 @@ if int(inputMain) == 5:
 if int(inputMain) == 6:
     #neofetch Installer
     InputUpdate = input("update system? \n")
-    if InputUpdate == "yes":
+    if InputUpdate == "yes" or InputUpdate == "Yes":
         os.system('sudo apt update && sudo apt upgrade')
     else:
         pass
     os.system('sudo apt install neofetch')
     askOpen = input("start neofetch?\n")
-    if askOpen == "yes":
+    if askOpen == "yes" or askOpen == "Yes":
         os.system('neofetch')
     else:
         pass
     AskClose = input("close installer? \n")
-    if AskClose == "yes":
+    if AskClose == "yes" or AskClose == "Yes":
+        os.system('clear')
+        exit()
+    else:
+        os.system('python3 Installer.py')
+
+
+if int(inputMain) == 7:
+    InputUpdate = input("update system? \n")
+    if InputUpdate == "yes" or InputUpdate == "Yes":
+        os.system('sudo apt update && sudo apt upgrade')
+    else:
+        pass
+    os.system('sudo apt install wget')
+    os.system('wget  ~/Minecraft.deb https://launcher.mojang.com/download/Minecraft.deb')
+    os.system('sudo apt install gdebi-core')
+    os.system('sudo gdebi Minecraft.deb')
+    AskClose = input("close installer? \n")
+    if AskClose == "yes" or AskClose == "Yes":
+        os.system('clear')
         exit()
     else:
         os.system('python3 Installer.py')
