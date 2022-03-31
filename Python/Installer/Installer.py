d6fbe328b1ab5b8457ac321c6f20d49be94f0235
justkevinyt11 <stephenskevin858@gmail.com>
2022-03-31 15:00:53 -0400

    Added minecraft bedrock

--- Python/Installer/Installer.py
+++ Python/Installer/Installer.py
@@ -1,12 +1,14 @@
 import os
 def main():
-    tools = ['OneX', 'VsCode', 'Obs', 'HTop', 'Tool-X', 'Neofetch', 'Minecraft']
+    tools = ['OneX', 'VsCode', 'Obs', 'HTop', 'Tool-X', 'Neofetch', 'Minecraft', 'Minecraft Bedrock Edition (needs restart) (FlatPack)']
+
 
     os.system('clear')
     print("tools")
     for index, tool in enumerate(tools, start=1):
         print (index, tool)
     print ('x Exit')
+
 
 main()
 inputMain = input("Choose Tool: \n")
@@ -106,6 +108,7 @@ if int(inputMain) == 5:
     else:
         os.system('python3 Installer.py')
 
+
 if int(inputMain) == 6:
     #neofetch Installer
     InputUpdate = input("update system? \n")
@@ -137,6 +140,23 @@ if int(inputMain) == 7:
     os.system('wget  ~/Minecraft.deb https://launcher.mojang.com/download/Minecraft.deb')
     os.system('sudo apt install gdebi-core')
     os.system('sudo gdebi Minecraft.deb')
+    AskClose = input("close installer? \n")
+    if AskClose == "yes" or AskClose == "Yes":
+        os.system('clear')
+        exit()
+    else:
+        os.system('python3 Installer.py')
+
+
+if int(inputMain) == 8:
+    InputUpdate = input("update system? \n")
+    if InputUpdate == "yes" or InputUpdate == "Yes":
+        os.system('sudo apt update && sudo apt upgrade')
+    else:
+        pass
+    os.system('sudo apt install flatpak')
+    os.system('sudo flatpak remote-add –if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo')
+    os.system('sudo flatpak install flathub io.mrarm.mcpelauncher')
     AskClose = input("close installer? \n")
     if AskClose == "yes" or AskClose == "Yes":
         os.system('clear')
